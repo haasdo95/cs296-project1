3777e729ac961927e46dae0eec713ae842fc9690
haasdo95 <gdou2@illinois.edu>
2017-04-15 21:44:17 -0500

    Almost Done

--- web/vis.js
+++ web/vis.js
@@ -1,27 +1,37 @@
 "use strict";
 
-var checkedColleges = Object.create(null);
+var data = null;
 
-$("#sb").on("change", function (event) {
-    $("#chosenYear").text(2000+Number($(this).val()));
-})
-
-$(".form-check-input").on("change", function (event) {
-    var chosen = event.target;
-    if (chosen.checked) { // newly checked
-        checkedColleges[chosen.getAttribute("college")] = true;
-        console.log(checkedColleges);
-    } else { // newly unchecked
-        delete checkedColleges[chosen.getAttribute("college")];
-        console.log(checkedColleges);
-    }
-})
+var checkedColleges = {
+    "KP " : true,
+    "KM " : true,
+    "KT " : true,
+    "KV " : true,
+    "LC " : true,
+    "KY " : true,
+    "KL " : true,
+    "KN " : true,
+    "KR " : true,
+    "KS " : true,
+    "LG " : true,
+    "NB " : true,
+    "KW " : true,
+    "LN " : true,
+    "LL " : true,
+    "KU " : true,
+    "LP " : true,
+    "LE " : true,
+    "LM " : true
+};
 
 /* Boilerplate jQuery */
 $(function() {
   $.get("res/uiuc_demographics_2005_15.csv")
    .done(function (csvData) {
-     var data = d3.csvParse(csvData);
+     data = d3.csvParse(csvData);
+     var tempData = data.filter(function (d) { // filtered by year
+         return data["Fall"] == "2015";
+     });
      visualize(data);
    })
   .fail(function(e) {
@@ -29,18 +39,51 @@ $(function() {
   });
 });
 
+$("#sb").on("change", function (event) {
+    $("#chosenYear").text(2000+Number($(this).val()));
+    if (!data) {
+        return;
+    }
+    console.log("Now the dict is: ", checkedColleges);
+    var tempData = data.filter(function (d) { // filtered by year
+        return (d["Fall"] == $("#chosenYear").text()) && checkedColleges[d["College"]];
+    });
+    $("svg").remove();
+    visualize(tempData);
+})
+
+$(".form-check-input").on("change", function (event) {
+    var chosen = event.target;
+    if (chosen.checked) { // newly checked
+        checkedColleges[chosen.getAttribute("college")+" "] = true;
+    } else { // newly unchecked
+        delete checkedColleges[chosen.getAttribute("college")+" "];
+    }
+    if (!data) {
+        return;
+    }
+    console.log("Now the dict is: ", checkedColleges);
+    var tempData = data.filter(function (d) { // filtered by year
+        return (d["Fall"] == $("#chosenYear").text()) && checkedColleges[d["College"]];
+    });
+    $("svg").remove();
+    visualize(tempData);
+})
+
 /* Visualize the data in the visualize function */
 var visualize = function(data) {
+  console.log("CALLED!");
   console.log(data);
+  if (!data.length) { // empty svg
+      var svg = d3.select("#chart")
+            .append("svg");
+      return;
+  }
   var majorCount = [];
-
-  data = data.filter(function (d) {
-      return d.Fall == "2015";
-  })
 
   var groupData = _.groupBy(data, "Major Name");
   groupData = _.toArray(groupData);
-  console.log(groupData);
+  // console.log(groupData);
   // groupData is now an array of arrays(a major)
   groupData.forEach(function (major) {
       var majorName = major[0]["Major Name"];
@@ -59,7 +102,6 @@ var visualize = function(data) {
   majorCount = majorCount.sort(function (m1, m2) {
       return m1.Total - m2.Total
   })
-  console.log(majorCount);
 
   var desiredNumOfMajor = [];
   desiredNumOfMajor.push(Math.floor(majorCount.length * 0.4));
@@ -73,8 +115,8 @@ var visualize = function(data) {
       acc.push( (acc.length > 0 ? acc[acc.length-1] : 0) + n);
       return acc
   }, [])
-  console.log(desiredNumOfMajor);
-  console.log(desiredNumOfMajorCumu);
+  // console.log(desiredNumOfMajor);
+  // console.log(desiredNumOfMajorCumu);
   var thresholds = [];
   var thres = 0;
   desiredNumOfMajor.forEach(function (num) {
@@ -129,4 +171,13 @@ var visualize = function(data) {
      .attr("fill", function (d,i) {
 
      })
+
+     for (var i = 6; i >= 0; i--) {
+     svg.append("ellipse")
+       .attr("cx",0)
+       .attr("cy",0)
+       .attr("rx",(width/2)*(i/6))
+       .attr("ry",(height/2)*(i/6)*6.5/6)
+       .attr("fill", "hsla(225, 100%, 50%, 0.1)");
+   }
 };
